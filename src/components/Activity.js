d62980456920579b61ed230e23e6b5537349e402
yunus kibar <yunuskibaar@gmail.com>
2023-09-08 11:37:56 +0300

    activity component updated; created pop up and profil componet added

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -2,21 +2,42 @@ import {Link, useNavigate} from 'react-router-dom';
 import './Activity.css';
 import ActivityDate from './ActivityDate';
 import Card from '../UI/Card';
+import ProfilPhoto from './ActivityProfilPhoto';
 
 function Activity(props){
     return(
         <>
-            <Link to=".." relative="path">
-                <Card className="activity-item">
-                    <ActivityDate date = {props.date}/>
-                    <div className="activity-item__description">
-                        {props.title}
+                
+                
+                    
+                    <Card className="activity-item">    
+                        <ProfilPhoto img />
+                      
+                        <div className="activity-item__description">
+                            {props.title}
+                            
+                        </div>
                         
-                    </div>
-                    <button className="activity-item__button">Katılma isteği</button>
+                            <div className='activity-box-time-and-invite'>
+                                <div className='activty-box-time'>
+                                <ActivityDate 
+                                date = {props.date}>  
+                                </ActivityDate>
+                                </div>
+                                <div className='activty-box-invite'>
+                                    
+                                <button 
+                                className="activity-item__button">Katılma isteği
+                                </button>
+                               
+                                </div>
+                               
+                            </div>
+                    
+                    
 
-                </Card>
-            </Link>
+                    </Card>
+           
             
             
         </>)
---
+++ src/components/ActivityProfilPhoto.js
@@ -0,0 +1,34 @@
+import "./ActivityProfilPhoto.css"
+
+
+
+
+import React, { useState } from 'react';
+import Modal from './Modal';
+
+function ProfilPhoto() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
+    return (
+        
+        <div className="profil-photo-popup">
+            <button className="profil-resmi" onClick={openModal}>
+                <img src="/image/img.jpg" alt=""></img>    
+            </button>
+            <Modal isOpen={isModalOpen} onClose={closeModal}>
+                <h2>Popup İçeriği</h2>
+                <p>Bu bir pop-up penceresidir.</p>
+            </Modal>
+        </div>
+    );
+}
+
+export default ProfilPhoto;
